9a675f5371571f1de9c867e54e8d854549cf33cc
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-04-15 09:27:37 +0200

    Add dotenv-safe and dotenv-cli

---
+++ util/database.ts
@@ -0,0 +1,30 @@
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
+
+config();
+
+declare module globalThis {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
+
+function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    sql = postgres();
+
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
+  return sql;
+}
+
+const sql = connectOneTimeToDatabase();
