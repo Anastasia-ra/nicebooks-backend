13b5bc12740f0c4443bab6df5687497ca96b8bdb
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-05-11 09:36:03 +0200

    Add functions to creater user

--- util/database.ts
+++ util/database.ts
@@ -1,6 +1,8 @@
+import camelcaseKeys from 'camelcase-keys';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku.js';
+import { User } from './types';
 
 setPostgresDefaultsOnHeroku();
 config();
@@ -30,3 +32,48 @@ function connectOneTimeToDatabase() {
 }
 
 const sql = connectOneTimeToDatabase();
+
+export async function deleteExpiredSessions() {
+  const sessions = await sql`
+  DELETE FROM
+    sessions
+  WHERE
+    expiry < NOW()
+  RETURNING
+    *
+  `;
+  return sessions.map((session) => camelcaseKeys(session));
+}
+
+export async function createSession(token: string, userId: number) {
+  const session = await sql`
+  INSERT INTO sessions
+    (token, user_id)
+  VALUES
+    (${token}, ${userId})
+  RETURNING
+    id,
+    token,
+    user_id
+  `;
+  await deleteExpiredSessions();
+  return camelcaseKeys(session[0]);
+}
+
+export async function createUser(
+  email: string,
+  username: string,
+  passwordHash: string,
+) {
+  const [user] = await sql<[User]>`
+    INSERT INTO users
+      (email, username, password_hash)
+    VALUES
+       (${email}, ${username}, ${passwordHash})
+    RETURNING
+       id,
+       email,
+       username
+  `;
+  return camelcaseKeys(user);
+}
